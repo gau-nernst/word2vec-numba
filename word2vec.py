09f98d94b162647822255698563db8f5a9773ead
Thien Tran <gau.nernst@yahoo.com.sg>
2023-02-22 10:58:55 +0800

    remove progress bar. add nogil

--- word2vec.py
+++ word2vec.py
@@ -1,5 +1,3 @@
-import time
-
 import numba as nb
 import numpy as np
 
@@ -14,7 +12,7 @@ def numba_logsigmoid(x):
     return -np.log(1 + np.exp(-x)) if x >= 0 else x - np.log(1 + np.exp(x))
 
 
-@nb.njit("int32(float64[::1], float64)")
+@nb.njit("int64(float64[::1], float64)")
 def numba_bisect_left(arr, x):
     lo, hi = 0, arr.shape[0]
     while hi > lo:
@@ -33,7 +31,7 @@ def axpy(a, x, y):
 
 
 @nb.njit
-def binary_cross_entropy(in_emb, out_emb, label, grad_in_emb, grad_out_emb, lr, return_loss):
+def binary_cross_entropy(in_emb, out_emb, label, grad_in_emb, grad_out_emb, lr, return_loss=False):
     z = np.vdot(in_emb, out_emb)
     g = (label - numba_sigmoid(z)) * lr  # this is -dloss / dz * lr
     axpy(g, out_emb, grad_in_emb)
@@ -42,25 +40,24 @@ def binary_cross_entropy(in_emb, out_emb, label, grad_in_emb, grad_out_emb, lr, 
 
 
 @nb.njit
-def skipgram_negative_sampling_step(input_embs, output_embs, in_idx, out_idx, noise_cdf, negative, lr, return_loss):
-    grad_in_emb = np.zeros(input_embs.shape[1], dtype=input_embs.dtype)
+def skipgram_negative_sampling_step(input_embs, output_embs, in_idx, out_idx, noise_cdf, negative, lr, grad_in_emb):
+    grad_in_emb[:] = 0.0
     in_emb = input_embs[in_idx]  # this is a view
     out_emb = output_embs[out_idx]
 
     # accumulate gradients for in_emb. directly update out_emb
-    loss = binary_cross_entropy(in_emb, out_emb, 1.0, grad_in_emb, out_emb, lr, return_loss)
+    binary_cross_entropy(in_emb, out_emb, 1.0, grad_in_emb, out_emb, lr)
     for _ in range(negative):
         noise_idx = numba_bisect_left(noise_cdf, np.random.rand())
         if noise_idx == out_idx:
             continue
         out_emb = output_embs[noise_idx]
-        loss += binary_cross_entropy(in_emb, out_emb, 0.0, grad_in_emb, out_emb, lr, return_loss)
+        binary_cross_entropy(in_emb, out_emb, 0.0, grad_in_emb, out_emb, lr)
 
     axpy(1.0, grad_in_emb, in_emb)
-    return loss
 
 
-@nb.njit
+@nb.njit(nogil=True)
 def skipgram_negative_sampling_train(
     stream: np.ndarray,
     input_embs: np.ndarray,
@@ -68,22 +65,14 @@ def skipgram_negative_sampling_train(
     noise_cdf: np.ndarray,
     window_size: int = 10,
     negative: int = 5,
-    lr: float = 0.025,
-    lr_anneal_interval: int = 10_000,
+    max_lr: float = 0.025,
+    min_lr: float = 0.0,
 ):
-    # NOTE: context predicts target, following Google's Word2Vec C code
-    # NOTE: computing loss is expensive
-    # only compute them for diagnostic and monitoring
-    interval = 10_000
-    current_lr = lr
-    with nb.objmode(time0="float64"):
-        time0 = time.time()
+    delta_lr = (max_lr - min_lr) / stream.shape[0]
+    grad_in_emb = np.empty(input_embs.shape[1], dtype=input_embs.dtype)  # allocate re-usable temp memory
 
     for stream_pos in range(stream.shape[0]):
-        if (stream_pos + 1) % lr_anneal_interval == 0:
-            current_lr = lr * (1 - stream_pos / stream.shape[0])
-            current_lr = max(current_lr, lr * 0.0001)
-
+        lr = max_lr - delta_lr * stream_pos
         truncated_window_size = np.random.randint(window_size) + 1
         window_start = max(stream_pos - truncated_window_size, 0)
         window_end = min(stream_pos + truncated_window_size + 1, stream.shape[0])
@@ -92,25 +81,14 @@ def skipgram_negative_sampling_train(
             if window_idx == stream_pos:
                 continue
 
-            loss = skipgram_negative_sampling_step(
+            # NOTE: context predicts target, following Google's Word2Vec C code
+            skipgram_negative_sampling_step(
                 input_embs,
                 output_embs,
                 stream[window_idx],  # context word
                 stream[stream_pos],  # center word
                 noise_cdf,
                 negative,
-                current_lr,
-                (stream_pos + 1) % interval == 0,
+                lr,
+                grad_in_emb,
             )
-
-        if (stream_pos + 1) % interval == 0:
-            with nb.objmode(time0="float64"):
-                progress = (stream_pos + 1) / stream.shape[0] * 100
-                throughput = int(interval / (time.time() - time0))
-                print(
-                    "\rlr: {:.2e}  Progress: {:.2f}%  Loss: {:.4f}  Words/sec: {:,}".format(
-                        current_lr, progress, loss, throughput
-                    ),
-                    end="",
-                )
-                time0 = time.time()
